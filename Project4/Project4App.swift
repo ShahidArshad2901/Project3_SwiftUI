687c2c7f1b8f7c89597da5a0fd7e14a894bea5fd
Shahid Arshad <shahid.arshad@devsinc.com>
2023-01-27 14:40:33 +0500

    Initial Commit

---
+++ Project4/Project4App.swift
@@ -0,0 +1,17 @@
+//
+//  Project4App.swift
+//  Project4
+//
+//  Created by Dev on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Project4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
